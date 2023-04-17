178122eef46bd7d7955d46593466870c0a1523e7
Chernovsky01 <a.laskin01@gmail.com>
2023-04-17 22:01:00 +0700

    Update Rotator.cs
    Upgrade

--- Rotator.cs
+++ Rotator.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
+    public float angleX;
+    public float angleY;
+    public float angleZ;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,13 +17,13 @@ public class Rotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Quaternion rotationX = Quaternion.AngleAxis(1, new Vector3(1, 0, 0));
+        Quaternion rotationX = Quaternion.AngleAxis(angleX, new Vector3(1, 0, 0));
         transform.rotation *= rotationX;
 
-        Quaternion rotationY = Quaternion.AngleAxis(2, new Vector3(0, 1, 0));
+        Quaternion rotationY = Quaternion.AngleAxis(angleY, new Vector3(0, 1, 0));
         transform.rotation *= rotationY;
 
-        Quaternion rotationZ = Quaternion.AngleAxis(3, new Vector3(0, 0, 1));
+        Quaternion rotationZ = Quaternion.AngleAxis(angleZ, new Vector3(0, 0, 1));
         transform.rotation *= rotationZ;
     }
 }
